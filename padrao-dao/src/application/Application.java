cc76c85517b1a6c69107c4a95908a262e5af003b
lia-oliveira <dev.gillian@gmail.com>
2025-03-01 09:42:20 -0300

    class Seller created

--- padrao-dao/src/application/Application.java
+++ padrao-dao/src/application/Application.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Application {
 
 	public static void main(String[] args) {
 		Department departamento = new Department(1, "Books");
 		System.out.print(departamento);	
-
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0);
+		System.out.println(seller);
 	}
 
 }
