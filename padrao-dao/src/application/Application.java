0d7db8560e85887e4fc7e99bab9b814a9dc2f05a
lia-oliveira <dev.gillian@gmail.com>
2025-03-01 09:30:48 -0300

    department class created

--- padrao-dao/src/application/Application.java
+++ padrao-dao/src/application/Application.java
@@ -1,9 +1,12 @@
 package application;
 
+import model.entities.Department;
+
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Department departamento = new Department(1, "Books");
+		System.out.print(departamento);	
 
 	}
 
